b3bc76f37785d6acd57938ea8f412f1fd06dcb09
jonaylg <jonaylg2003@gmail.com>
2025-01-16 12:14:17 +0100

    controlador_coches actualizado

--- miPrimeraAPI/web/rutas_juegos.py
+++ miPrimeraAPI/web/rutas_juegos.py
@@ -2,7 +2,7 @@ from flask import request, session
 import json
 import decimal
 from __main__ import app
-import controlador_juegos
+import miPrimeraAPI.web.controlador_coches as controlador_coches
 
 class Encoder(json.JSONEncoder):
     def default(self, obj):
@@ -10,12 +10,12 @@ class Encoder(json.JSONEncoder):
 
 @app.route("/juegos",methods=["GET"])
 def juegos():
-    juegos,code= controlador_juegos.obtener_juegos()
+    juegos,code= controlador_coches.obtener_juegos()
     return json.dumps(juegos, cls = Encoder),code
 
 @app.route("/juego/<id>",methods=["GET"])
 def juego_por_id(id):
-    juego,code = controlador_juegos.obtener_juego_por_id(id)
+    juego,code = controlador_coches.obtener_juego_por_id(id)
     return json.dumps(juego, cls = Encoder),code
 
 @app.route("/juegos",methods=["POST"])
@@ -23,7 +23,7 @@ def guardar_juego():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         juego_json = request.json
-        ret,code=controlador_juegos.insertar_juego(juego_json["nombre"], juego_json["descripcion"], float(juego_json["precio"]), juego_json["foto"])
+        ret,code=controlador_coches.insertar_juego(juego_json["nombre"], juego_json["descripcion"], float(juego_json["precio"]), juego_json["foto"])
     else:
         ret={"status":"Bad request"}
         code=401
@@ -31,7 +31,7 @@ def guardar_juego():
 
 @app.route("/juegos/<id>", methods=["DELETE"])
 def eliminar_juego(id):
-    ret,code=controlador_juegos.eliminar_juego(id)
+    ret,code=controlador_coches.eliminar_juego(id)
     return json.dumps(ret), code
 
 @app.route("/juegos", methods=["PUT"])
@@ -39,7 +39,7 @@ def actualizar_juego():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         juego_json = request.json
-        ret,code=controlador_juegos.actualizar_juego(juego_json["id"],juego_json["nombre"], juego_json["descripcion"], float(juego_json["precio"]),juego_json["foto"])
+        ret,code=controlador_coches.actualizar_juego(juego_json["id"],juego_json["nombre"], juego_json["descripcion"], float(juego_json["precio"]),juego_json["foto"])
     else:
         ret={"status":"Bad request"}
         code=401
