cbff80d30eec239b9ca794518e4a2434d35d48b0
jonaylg <jonaylg2003@gmail.com>
2025-01-16 11:09:09 +0100

    base de datos

--- miPrimeraAPI/Juegos.sql
+++ miPrimeraAPI/Juegos.sql
@@ -1,8 +1,10 @@
-CREATE DATABASE IF NOT EXISTS PEPS;
-USE PEPS;
-CREATE TABLE juegos(
+CREATE DATABASE IF NOT EXISTS CARROS;
+USE CARROS;
+CREATE TABLE coche(
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
+    matricula VARCHAR(255) NOT NULL,
+    marca VARCHAR(255) NOT NULL,
+    modelo VARCHAR(255) NOT NULL,
     descripcion VARCHAR(255) NOT NULL,
     precio DECIMAL(9,2) NOT NULL,
 	foto VARCHAR(255)
@@ -14,3 +16,9 @@ CREATE TABLE usuarios(
     fechaUltimoAcceso DATE
 );
 INSERT INTO `usuarios` (`usuario`, `clave`, `perfil`, `fechaUltimoAcceso`) VALUES ('root', '1234', 'admin', '2022-03-01');
+INSERT INTO coche (matricula, marca, modelo, descripcion, precio, foto) VALUES
+('1234ABC', 'Toyota', 'Corolla', 'Coche compacto y eficiente', 15000.00, 'toyota.jpg'),
+('5678DEF', 'Honda', 'Civic', 'Sedán confiable con buen rendimiento', 18000.00, 'honda.jpg'),
+('9012GHI', 'Ford', 'Mustang', 'Coche deportivo con alto rendimiento', 35000.00, 'ford.jpg'),
+('3456JKL', 'Chevrolet', 'Camaro', 'Muscle car con diseño elegante', 33000.00, 'chevrolet.jpg'),
+('7890MNO', 'BMW', '320i', 'Sedán de lujo con características avanzadas', 40000.00, 'bmw.jpg');
